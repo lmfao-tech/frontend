c9e714209a0c436e353d14a7aeb182456081e37c
kekda-py <yashverma30115@gmail.com>
2022-06-28 22:02:43 +0530

    implement oauth

--- src/components/ProfileBar/Profile.tsx
+++ src/components/ProfileBar/Profile.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Avatar, Button } from 'flowbite-react';
+import { signIn, useSession } from "next-auth/react";
+import { useEffect } from 'react';
 
-function Profile() {
+export default function Profile() {
+
+  const { data: session } = useSession();
+
+  let av = session?.user?.image;
+  if (av) {
+    av = (av.slice(0,-11)) + ".jpg";
+  } else {
+    av = ""
+  }
+
   return (
-    <div className='h-full'>
-      
+    <div className=''>
+      {session ? (
+        <div className='flex flex-col ava px-5 py-10 items-center'>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img 
+            src={av}
+            className='rounded-full'
+            alt='avatar'
+            width="100"
+          />
+        </div>
+      ) : (
+        <div className='flex flex-col px-10 gap-3 justify-center min-h-screen items-center'>
+          <h1 className='text-3xl font-bold main-heading'>
+            <span className='bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text' style={{WebkitTextFillColor : "transparent"}}>LMFAO</span>
+            .tech
+          </h1>
+          <p className='text-center text-sm'>A content discovery platform where you can find the best memes across twitter, follow memers and stay for a good laugh!</p>
+          <Button
+            outline size="lg"
+            gradientDuoTone="purpleToPink"
+            onClick={() => signIn('twitter')}
+          >
+            Login
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
-
-export default Profile
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import TwitterProvider from "next-auth/providers/twitter";
 
 // Prisma adapter for NextAuth, optional and can be removed
 // import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -9,9 +9,10 @@ export default NextAuth({
   // Configure one or more authentication providers
   // adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID!,
+      clientSecret: process.env.TWITTER_SECRET!,
+      version: '2.0' 
     }),
     // ...add more providers here
   ],
