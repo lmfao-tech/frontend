2de6ac2cff9cb9131b5632a40b05cc3c42167736
Dhravya <dhravyashah@gmail.com>
2022-07-08 15:05:47 +0530

    WHAT

--- src/components/Feed/FeedGrid.tsx
+++ src/components/Feed/FeedGrid.tsx
@@ -1,7 +1,9 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import FeedPost from "./FeedPost";
 import Masonry from "react-masonry-css";
 import Post from "~/types/Post";
+import InfiniteScroll from "react-infinite-scroll-component";
+import useSWR from "swr";
 
 const breakpointColumnsObj = {
   default: 2,
@@ -9,29 +11,37 @@ const breakpointColumnsObj = {
   640: 1,
 };
 
-function FeedGrid() {
-  
-  const [posts, setPosts] = React.useState<Post[]>([]);
-  
-  useEffect(() => {
-    fetch("/api/getMemes")
-      .then(res => res.json())
-      .then(data => {
-        setPosts(data['meme_stream']);
-      }
-    );
-  }
-  , []);
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
+function FeedGrid() {
+  const [posts, setPosts] = React.useState<Post[]>([]);
+  const [postsShown, setPostsShown] = React.useState<Post[]>([]);
+  const [currentNumber, setCurrentNumber] = React.useState<number>(0);
+
+  const { data, error } = useSWR("/api/getMemes", fetcher);
+
+  useEffect(() => {
+    if (data) {
+      setPosts(data["meme_stream"]);
+      setPostsShown(data["meme_stream"].slice(0, 10));
+    }
+  }
+  , [data]);
+
+  
+
+  if (!data) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
   return (
-    <Masonry
-      breakpointCols={breakpointColumnsObj}
-      className="flex w-full h-full overflow-auto scrollbar-thin scrollbar-thumb-slate-200"
-    >
-      {posts.map((post) => (
-        <FeedPost key={post.tweet_created_at} post={post} />
-      ))}
-    </Masonry>
+
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="flex w-full h-full overflow-auto scrollbar-thin scrollbar-thumb-slate-200"
+      >
+        {posts.map((post) => (
+          <FeedPost key={post.tweet_created_at} post={post} />
+        ))}
+      </Masonry>
   );
 }
 
--- src/pages/api/getMemes.ts
+++ src/pages/api/getMemes.ts
@@ -5,21 +5,32 @@ type Data = any;
 let current_memes = {"meme_stream":[]};
 let last_updated_timestamp = 0;
 
-const shuffle_memes = (memes:any) => {
-  let new_memes = [];
-  while (memes.length > 0) {
-    let index = Math.floor(Math.random() * memes.length);
-    new_memes.push(memes[index]);
-    memes.splice(index, 1);
+function shuffle(array: any[]) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
-  return new_memes;
+
+  return array;
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
   if (Date.now() - last_updated_timestamp < 120000) {
     console.log("Returning current memes");
-    res.status(200).json(current_memes);  
+    const shuffled = shuffle(current_memes.meme_stream);
+    res.status(200).json({"meme_stream": shuffled});  
   } else {
     fetch("https://api.lmfao.tech/get_memes")
       .then((res) => res.json())
