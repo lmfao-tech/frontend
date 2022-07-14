81aae38f649fde7aa2ec6af6c017368d575112ea
Dhravya <dhravyashah@gmail.com>
2022-07-14 19:37:18 +0530

    added icons and manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Script from "next/script";
 import { NextPage } from "next";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
@@ -12,6 +13,11 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
   return (
     <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/icons/maskable.png" />
+        <link rel="apple-touch-icon" href="/icons/maskable.png"></link>
+      </Head>
       {getLayout(<Component {...pageProps} />)}
       <Script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></Script>
     </SessionProvider>
