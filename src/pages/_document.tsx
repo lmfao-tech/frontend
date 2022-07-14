15876010fb54cd224c0b13bd0cf5540ac2f6e6e0
Dhravya <dhravyashah@gmail.com>
2022-07-14 16:47:21 +0530

    added umami analytics

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="ff7c1c8b-8ff2-4e72-828c-e140b07175a8"
+          src="https://umami.dhravya.dev/umami.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
