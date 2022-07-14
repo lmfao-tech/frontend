eca077b8b329dd066e1035a2cc87a34511ce00c4
Dhravya <dhravyashah@gmail.com>
2022-07-14 14:24:53 +0530

    PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
   reactStrictMode: false,
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
 module.exports = nextConfig
