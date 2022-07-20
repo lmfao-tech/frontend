e590994f8a33bb0f3d08605b1c2cd8aba8b4d913
Dhravya <dhravyashah@gmail.com>
2022-07-20 20:57:50 +0530

    made leaderboard a little bit responsive

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -26,7 +26,7 @@ function Leaderboard() {
     <div>
       <div className="w-full rounded-2xl py-3 bg-slate-200 dark:bg-slate-500/30 dark:text-white">
         <h1 className="text-center text-lg">Leaderboard</h1>
-        <div className="flex py-2 flex-col gap-1 overflow-y-scroll scrollbar-thin h-48">
+        <div className="flex py-2 flex-col gap-1 overflow-y-scroll scrollbar-thin h-48 xl:h-72 2xl:h-96 ">
             {top10.map((user, index) => (
                 <LeaderboardIcon key={index} rank={index + 1} coins={user.coins} name={user.name} avatar={user.avatar} />
             ))}
