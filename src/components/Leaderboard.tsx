47d102b35a9a70f1f5fd4099ee3870517a6704da
kekda-py <yashverma30115@gmail.com>
2022-08-05 18:49:01 +0530

    shift LeaderBoard and element into single file

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
-import LeaderboardIcon from './LeaderboardElement';
+import { Avatar } from 'flowbite-react'
+import logo_white from "~/../public/logo-white.png";
+import logo_black from "~/../public/logo-black.png";
+import { useAtom } from 'jotai';
+import darkModeAtom from '~/atoms/darkmode';
+import Image from "next/image"
 
+function LeaderboardIcon({coins,rank, name, avatar}: {coins: number,rank:number, name:string, avatar:string}) {
+  
+  const [darkMode, setDarkMode] = useAtom(darkModeAtom);
+
+  return (
+    <div>
+      <div className="flex items-center gap-2 px-5">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500">
+          #{rank}
+        </span>
+        <span>
+          <Avatar img={avatar} rounded={true} alt="avatar" size="sm" />
+        </span>
+        <span>{name}</span>
+        <span className="flex ml-auto">
+          <Image src={darkMode ? logo_white : logo_black} width={25} height={25} alt="LMFAO coins"/> {coins}</span>
+      </div>
+    </div>
+  );
+}
 
 function Leaderboard() {
 
