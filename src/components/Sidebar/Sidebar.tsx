2ea13843add179fa352fc99e054f25fbd859f5e7
yxshv <yashverma30115@gmail.com>
2022-08-07 12:47:55 +0530

    new component navItem

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -12,6 +12,37 @@ import Link from "next/link";
 import { useHaha } from "~/contexts/HahaContext";
 import { useRouter } from "next/router";
 
+interface Props {
+  tip: string;
+  icon: JSX.Element[] | JSX.Element;
+  route: string;
+}
+
+function Item({ tip, icon, route }: Props) {
+
+  const router = useRouter();
+
+  const currentPath = router.pathname;
+  const isThis = currentPath === route;
+
+  return (
+    <Link href={route}>
+      <div
+        className={`rounded-full flex justify-center items-center w-12 h-12 ${isThis && "dark:bg-slate-700/50 flex justify-center items-center w-12 h-12 bg-slate-400/20"
+          }`}
+      >
+        <SidebarIcon
+          icon={icon}
+          data-tip={tip}
+          data-place="right"
+        />
+      </div>
+    </Link>
+  )
+
+}
+
+
 function Sidebar() {
   const { mod } = useHaha();
   const router = useRouter();
@@ -26,141 +57,84 @@ function Sidebar() {
     <div className="bg-white dark:bg-[#242424]  z-10">
       {/* Mobile */}
       <div className="fixed bottom-0 flex items-center justify-between w-full h-20 px-8 bg-white md:hidden dark:bg-[#242424] drop-shadow-2xl">
-        <Link href="/">
-          <div
-            className={`rounded-full ${
-              isHome && "dark:bg-slate-700/50 bg-slate-400/20"
-            }`}
-          >
-            <SidebarIcon
-              icon={<HomeIcon className="w-6 h-6 text-cyan-500" />}
+      <Item
+            route="/"
+            tip="Home"
+            icon={<HomeIcon className="w-6 h-6 text-cyan-500" />}
+          />
+
+          <Item
+            route="/community"
+            tip="Community"
+            icon={<UsersIcon className="w-6 h-6 text-blue-400" />}
+          />
+
+          <Item
+            route="/create"
+            tip="upload"
+            icon={<PlusIcon className="w-6 h-6 text-gray-400" />}
+          />
+          
+          <SidebarIcon
+            icon={<UserIcon className="w-6 h-6 text-yellow-400" />}
+            data-tip="Profile"
+          />
+
+          <Item
+            route="/dash"
+            tip="Dashboard"
+            icon={<DesktopComputerIcon className="w-6 h-6 text-gray-400" />}
+          />
+
+          {mod && (
+            <Item
+              route="/mod"
+              tip="Moderation"
+              icon={<FilterIcon className="w-6 h-6 text-red-500" />}
             />
-          </div>
-        </Link>
-
-        <Link href="/community">
-          <div
-            className={`rounded-full ${
-              isCommunity && "dark:bg-slate-700/50 bg-slate-400/20"
-            }`}
-          >
-            <SidebarIcon
-              icon={<UsersIcon className="w-6 h-6 text-blue-400" />}
-            />
-          </div>
-        </Link>
-
-        <Link href="/create">
-          <div
-            className={`rounded-full ${
-              isCreate && "dark:bg-slate-700/50 bg-slate-400/20"
-            }`}
-          >
-            <SidebarIcon
-              icon={<PlusIcon className="w-6 h-6 text-gray-400" />}
-            />
-          </div>
-        </Link>
-
-        {mod && (
-          <Link href="/mod">
-            <div>
-              <SidebarIcon
-                icon={<FilterIcon className="w-6 h-6 text-red-500" />}
-              />
-            </div>
-          </Link>
-        )}
-
-        <SidebarIcon icon={<UserIcon className="w-6 h-6 text-yellow-400" />} />
-
-        <Link href="/dash">
-          <div
-            className={`rounded-full ${
-              isDashboard && "dark:bg-slate-700/50 bg-slate-400/20"
-            }`}
-          >
-            <SidebarIcon
-              icon={<DesktopComputerIcon className="w-6 h-6 text-gray-400" />}
-            />
-          </div>
-        </Link>
+          )}
       </div>
 
       {/* Larger devices */}
       <div className="sticky top-0 flex-col items-center justify-center hidden min-h-screen border-gray-300 shadow-md md:flex w-28 border-1">
         <div className="ml-10 space-y-7">
-          <Link href="/">
-            <div
-              data-tip="Home"
-              data-place="right"
-              className={`rounded-full ${
-                isHome && "dark:bg-slate-700/50 bg-slate-500/10"
-              }`}
-            >
-              <SidebarIcon
-                icon={<HomeIcon className="w-6 h-6 text-cyan-500" />}
-              />
-            </div>
-          </Link>
+          <Item
+            route="/"
+            tip="Home"
+            icon={<HomeIcon className="w-6 h-6 text-cyan-500" />}
+          />
 
-          <Link href="/community">
-            <div
-              data-tip="Community"
-              data-place="right"
-              className={`rounded-full ${
-                isCommunity && "dark:bg-slate-700/50 bg-slate-500/10"
-              }`}
-            >
-              <SidebarIcon
-                icon={<UsersIcon className="w-6 h-6 text-blue-400" />}
-              />
-            </div>
-          </Link>
+          <Item
+            route="/community"
+            tip="Community"
+            icon={<UsersIcon className="w-6 h-6 text-blue-400" />}
+          />
 
           <SidebarIcon
             icon={<UserIcon className="w-6 h-6 text-yellow-400" />}
             data-tip="Profile"
           />
-          <Link href="/dash">
-            <div
-              data-tip="Dashboard"
-              data-place="right"
-              className={`rounded-full ${
-                isDashboard && "dark:bg-slate-700/50 bg-slate-500/10"
-              }`}
-            >
-              <SidebarIcon
-                icon={<DesktopComputerIcon className="w-6 h-6 text-gray-400" />}
-                data-tip="Dashboard"
-              />
-            </div>
-          </Link>
-          <Link href="/create">
-            <div
-              data-tip="Create"
-              data-place="right"
-              className={`rounded-full ${
-                isCreate && "dark:bg-slate-700/50 bg-slate-500/10"
-              }`}
-            >
-              <SidebarIcon
-                icon={<PlusIcon className="w-6 h-6 text-gray-400" />}
-                data-tip="Upload"
-              />
-            </div>
-          </Link>
+
+          <Item
+            route="/dash"
+            tip="Dashboard"
+            icon={<DesktopComputerIcon className="w-6 h-6 text-gray-400" />}
+          />
+
+          <Item
+            route="/create"
+            tip="upload"
+            icon={<PlusIcon className="w-6 h-6 text-gray-400" />}
+          />
 
           {mod && (
-            <Link href="/mod">
-              <div>
-                <SidebarIcon
-                  icon={<FilterIcon className="w-6 h-6 text-red-500" />}
-                  data-tip="Moderation"
-                />
-              </div>
-            </Link>
+            <Item
+              route="/mod"
+              tip="Moderation"
+              icon={<FilterIcon className="w-6 h-6 text-red-500" />}
+            />
           )}
+
         </div>
 
         {/* TODO: Implement Sidebar toggle  */}
